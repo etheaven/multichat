2cf7e125768e9152604a72e210490e210fd4a37a
Simon <mojweb123@gmail.com>
2024-07-20 17:39:03 +0200

    Removed unsupported content encoding options from cURL request.

--- kick-proxy.php
+++ kick-proxy.php
@@ -19,11 +19,9 @@ curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) A
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Language: en-US,en;q=0.5',
-    'Accept-Encoding: gzip, deflate, br',
     'Connection: keep-alive',
     'Upgrade-Insecure-Requests: 1'
 ]);
-curl_setopt($ch, CURLOPT_ENCODING, ''); // Handle gzip/deflate content encoding
 
 $response = curl_exec($ch);
 
